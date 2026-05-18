f3432e06e6e0632847de067d5cf39533f82046d3
victorjudysen <judysen.victor@gmail.com>
2026-05-18 11:18:00 +0300

    fix: Akaunti tab uses context.go to account screen to preserve navbar

--- lib/features/trainer/screens/trainer_dashboard_screen.dart
+++ lib/features/trainer/screens/trainer_dashboard_screen.dart
@@ -730,7 +730,7 @@ class _TrainerDashboardScreenState extends State<TrainerDashboardScreen>
                     const Color(0xFF7B3A10), () => _tabController.animateTo(3)),
                 const SizedBox(width: 10),
                 _buildQuickAction(Icons.person_outline_rounded, 'Akaunti',
-                    const Color(0xFF3D1800), () => context.push('/profile')),
+                    const Color(0xFF3D1800), () => context.go('/account')),
                 const SizedBox(width: 10),
                 _buildQuickAction(Icons.account_balance_wallet_outlined, 'Mkoba',
                     const Color(0xFF7B3A10), () => context.push('/wallet')),
@@ -2194,7 +2194,7 @@ class _TrainerDashboardScreenState extends State<TrainerDashboardScreen>
                                                 color: Colors.white,
                                                 size: 24.r),
                                             onPressed: () =>
-                                                context.push('/profile'),
+                                                context.go('/account'),
                                             tooltip: 'Mipangilio',
                                           ),
                                         ],
