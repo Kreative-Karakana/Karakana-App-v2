a8c7610d625241a2d03c9657235c2a25aec754c3
victorjudysen <judysen.victor@gmail.com>
2026-05-23 18:15:54 +0300

    feat: add Responsive utility class

---
+++ lib/core/utils/responsive.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+
+class Responsive {
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 600;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 600;
+
+  static double h(BuildContext context, double percent) =>
+      MediaQuery.of(context).size.height * percent;
+
+  static double w(BuildContext context, double percent) =>
+      MediaQuery.of(context).size.width * percent;
+
+  static double topPadding(BuildContext context) =>
+      MediaQuery.of(context).padding.top;
+
+  static double bottomPadding(BuildContext context) =>
+      MediaQuery.of(context).padding.bottom;
+
+  static double sp(BuildContext context, double size) =>
+      size * MediaQuery.of(context).textScaleFactor.clamp(0.85, 1.2);
+}
